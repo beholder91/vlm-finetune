50ea0f3c44bab1c69129b44cf1f8e0d9634788a3
Chen Kehao <kkehaochen@gmail.com>
2025-05-12 17:57:42 +0800

    1

--- train.sh
+++ train.sh
@@ -1,6 +1,15 @@
-ACCELERATE_LOG_LEVEL=DEBUG \
-TRANSFORMERS_VERBOSITY=debug \
-DEEPSPEED_LOGS=debug \
-NCCL_DEBUG=INFO \
-DS_DEBUG=1 \
+# NCCL 通信调试
+export NCCL_DEBUG=INFO
+export NCCL_ASYNC_ERROR_STACK=1
+
+# DeepSpeed 日志
+export DEEPSPEED_LOG_LEVEL=debug
+
+# Accelerate 日志
+export ACCELERATE_LOG_LEVEL=debug
+
+# Transformers 日志
+export TRANSFORMERS_VERBOSITY=error
+export HF_HUB_DISABLE_LOAD_WARNINGS=1
+
 accelerate launch --config_file deepspeed_zero3.yaml train.py
